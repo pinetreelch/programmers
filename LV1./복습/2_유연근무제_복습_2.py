bd65323af91dc5aee59285f6bb27b951868ec11a
pinetreelch <chanho.lee0215@gmail.com>
2025-03-04 23:44:17 +0900

    20250304 try 4

--- LV1./복습/2_유연근무제_복습_2.py
+++ LV1./복습/2_유연근무제_복습_2.py
@@ -1,21 +1,30 @@
 def solution(schedules, timelogs, startday):
 
-    # 요일 만들기
-    days = []
+    result = []
 
-    for i in range(startday, startday + 7):
-        if i > 7:
-            days.append(i - 7)
-        else:
-            days.append(i)
+    start_day_set = startday
 
-    print(days) 
+    for log in timelogs:
+        for time in schedules:
+            # time  => 직원의 희망 시간
 
-    for time in schedules:
+            time_late = time + 10  # 직원들 이 시간 보다 늦으면 지각임
+            result_p = []
+            startday = start_day_set
 
-        # time  => 직원의 희망 시간
-        time_late = time + 10  # 직원들 이 시간 보다 늦으면 지각임
+            for log_real in log:
+                if startday == 6 or startday == 7:
+                    result_p.append("O")
+                    startday += 1
+                    continue
+                else:
+                    if log_real <= time_late:
+                        result_p.append("O")
+                    else:
+                        result_p.append("X")
+                    startday += 1
 
+            print(result_p)
         break
 
 
